3273f7fb8e0feafc9cc862de99ca1d39422b53ae
Joseph Kelley <josephkelley91@gmail.com>
2024-07-25 09:24:32 -0400

    Search | Adds new initial search field to the App component.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <input className="App-header-search-field" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
