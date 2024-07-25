ab224769af17b5fc333136cf69469e5092d37371
Joseph Kelley <josephkelley91@gmail.com>
2024-07-25 14:57:11 -0400

    Configured the background color of the page to change based on which filtered search option is selected.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     { option_id: 3, option_label: 'Yellow', option_value: 'yellow' }
   ]);
   const [searchValue, setSearchValue] = useState('');
+  const [selectedClassname, setSelectedClassname] = useState('App-header App-header__blue');
 
   function onSearchValueChanged(changeEvent) {
     setSearchValue(changeEvent.target.value);
@@ -16,13 +17,30 @@ function App() {
 
   const searchOptionsToRender = searchOptions.filter(option => option.option_label.toLowerCase().includes(searchValue.toLowerCase()));
 
+  function onOptionSelect(option) {
+    const selectedOptionId = option.option_id;
+    if (selectedOptionId === 0) {
+      setSelectedClassname('App-header App-header__blue');
+    }
+    if (selectedOptionId === 1) {
+      setSelectedClassname('App-header App-header__red');
+    }
+    if (selectedOptionId === 2) {
+      setSelectedClassname('App-header App-header__orange');
+    }
+    if (selectedOptionId === 3) {
+      setSelectedClassname('App-header App-header__yellow');
+    }
+    console.log(selectedClassname);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className={selectedClassname}>
         <input className="App-header-search-field" value={searchValue} onChange={onSearchValueChanged} />
         <ul>
           {searchOptionsToRender.map(option =>
-            <li key={option.option_id}>
+            <li className="App-header-search-option" key={option.option_id} onClick={() => onOptionSelect(option)}>
               {option.option_label}
             </li>
           )}
