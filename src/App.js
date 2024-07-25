d3bd251ef5fac658888a14b069a816d45dab0e23
Joseph Kelley <josephkelley91@gmail.com>
2024-07-25 13:51:42 -0400

    Adds logic to filter list options by utilizing React state changes. Clean up.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
-  const searchOptions = [
+  const [searchOptions, setSearchOptions] = useState([
     { option_id: 0, option_label: 'Blue', option_value: '#282c34' },
     { option_id: 1, option_label: 'Red', option_value: 'red' },
     { option_id: 2, option_label: 'Orange', option_value: 'orange' },
     { option_id: 3, option_label: 'Yellow', option_value: 'yellow' }
-  ];
-  const searchOptionsList = searchOptions.map(option =>
-    <li key={option.option_id}>
-      {option.option_label}
-    </li>
-  );
+  ]);
+  const [searchValue, setSearchValue] = useState('');
+
+  function onSearchValueChanged(changeEvent) {
+    setSearchValue(changeEvent.target.value);
+  };
+
+  const searchOptionsToRender = searchOptions.filter(option => option.option_label.toLowerCase().includes(searchValue.toLowerCase()));
 
   return (
     <div className="App">
       <header className="App-header">
-        <input className="App-header-search-field" />
-        <ul>{searchOptionsList}</ul>
+        <input className="App-header-search-field" value={searchValue} onChange={onSearchValueChanged} />
+        <ul>
+          {searchOptionsToRender.map(option =>
+            <li key={option.option_id}>
+              {option.option_label}
+            </li>
+          )}
+        </ul>
       </header>
     </div>
   );
