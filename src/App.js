b42757a5cd282302aa598d7b038ea773d31ca779
Joseph Kelley <josephkelley91@gmail.com>
2024-07-25 09:40:04 -0400

    Adds search options. Modifies page title value to reflect purpose of application.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const searchOptions = [
+    { option_id: 0, option_value: '#282c34' },
+    { option_id: 1, option_value: 'red' },
+    { option_id: 2, option_value: 'orange' },
+    { option_id: 3, option_value: 'yellow' }
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
